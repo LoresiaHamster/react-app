1088bc172c5ea7609e7f0b15b73e563173d4784e
LoresiaHamster <tomokiayuna@gmail.com>
2024-03-08 19:06:32 +0300

    First Commit

---
+++ src/Message.tsx
@@ -0,0 +1,12 @@
+
+function Message () {
+    //jsx: JavaScript XML
+
+    const name = 'Asya';
+
+    if (name)
+        return <h1>Hello {name}!</h1>;
+    return <h1>Hello World!</h1>
+}
+
+export default Message;
